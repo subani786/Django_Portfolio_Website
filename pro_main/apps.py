94ce00d6a019811d9c85165e4142edfe73bf8053
subani786 <dmabusubani@gmail.com>
2022-05-16 20:53:34 +0530

    first commit

---
+++ pro_main/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ProMainConfig(AppConfig):
+    name = 'pro_main'
---
+++ pro_main/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def pro_main(request):
+
+    
+    return render(request,'index.html') 
